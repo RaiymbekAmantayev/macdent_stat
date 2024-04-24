c874d5632edc090971a0856920c88b9f3d67ed42
Amantatev Raiymbek <raiymbek.amanatyev@narxoz.kz>
2024-04-24 17:11:27 +0600

    send to ui

--- src/pages/Layout/DoctorTotal.vue
+++ src/pages/Layout/DoctorTotal.vue
@@ -1,134 +1,120 @@
 <template>
-  <div class="content">
-    <div class="md-layout">
-      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
-        <chart-card
-            :chart-data="emailsSubscriptionChart.data"
-            :chart-options="emailsSubscriptionChart.options"
-            :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
-            :chart-type="'Bar'"
-            data-background-color="red"
-        >
-          <template slot="content">
-            <h4 class="title">Email Subscription</h4>
-            <p class="category">Last Campaign Performance</p>
-          </template>
-
-          <template slot="footer">
-            <div class="stats">
-              <md-icon>access_time</md-icon>
-              updated 10 days ago
-            </div>
-          </template>
-        </chart-card>
-      </div>
-
-      <div
-          class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-50"
-      >
-        <md-card>
-          <md-card-header data-background-color="orange">
-            <h4 class="title">Employees Stats</h4>
-            <p class="category">New employees on 15th September, 2016</p>
-          </md-card-header>
-          <md-card-content>
-            <ordered-table table-header-color="orange"></ordered-table>
-          </md-card-content>
-        </md-card>
-      </div>
+  <div>
+    <div class="chart-container">
+      <canvas ref="chart1" width="850" height="300"></canvas>
+    </div>
+    <div class="chart-info">
+      <!-- Здесь можете добавить любую информацию, которую хотите отображать -->
     </div>
   </div>
 </template>
 
 <script>
-import {ChartCard, OrderedTable,} from "@/components";
+import Chart from 'chart.js/auto';
 
 export default {
   props: {
-    doctorTotal: Object,
-    busy_time:Object,
-    countCheck:Object
+    countCheck: Object,
+    doctorTotal:Object,
+    busy_Time: Object
   },
-  data() {
-    return {
-      emailsSubscriptionChart: {
-        data: {
-          doctorTotals: {},
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De",
-          ],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895],
-          ],
-        },
-        options: {
-          axisX: {
-            showGrid: false,
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0,
-          },
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function (value) {
-                  return value[0];
-                },
-              },
-            },
-          ],
-        ],
-      },
-    };
-  },
-  components: {
-    ChartCard,
-    OrderedTable,
-  },
-  async mounted() {
-    // console.log("records: ", this.doctorTotal);
-    // console.log("busy_time_from_DrTT: ", this.busy_time)
-    // console.log("countCheck: ", this.countCheck)
+  mounted() {
+    this.createChart('chart1', [], []);
   },
   watch: {
-    '$props.doctorTotal': {
-      handler(newDoctorTotal, oldDoctorTotal) {
-        console.log('Данные о докторах обновлены:', newDoctorTotal);
-      },
-      deep: true
+    countCheck(newCountCheck) {
+      this.updateChart('chart1', Object.keys(newCountCheck), Object.values(newCountCheck));
+    }
+  },
+  methods: {
+    createChart(canvasId, labels, data) {
+      const ctx = this.$refs[canvasId].getContext('2d');
+      this.chartInstance = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [{
+            label: '',
+            data: data,
+            backgroundColor: '#fff',
+            borderColor: '#fff',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          indexAxis: 'y',
+          scales: {
+            yAxes: [{
+              gridLines: {
+                display: false,
+                color: 'white'
+              },
+              ticks: {
+                fontColor: 'white',
+                beginAtZero: true
+              }
+            }],
+            xAxes: [{
+              gridLines: {
+                display: false,
+                color: 'white'
+              },
+              ticks: {
+                fontColor: 'white'
+              }
+            }],
+          },
+          legend: {
+            labels: {
+              fontColor: 'white'
+            }
+          },
+          barPercentage: 0.3,
+          categoryPercentage: 0.5
+        }
+      });
     },
-    '$props.busy_time':{
-      handler(newBusyTime, oldBusyTime) {
-        // console.log('Данные о записей обновлены:', newBusyTime);
-      },
-      deep:true
-    },
-    '$props.countCheck':{
-      handler(newCount, oldCount) {
-        // console.log('Данные о количестве чеков обновлены:', newCount);
-      },
-      deep:true
+    updateChart(canvasId, labels, data) {
+      if (this.chartInstance) {
+        this.chartInstance.data.labels = labels;
+        this.chartInstance.data.datasets[0].data = data;
+        this.chartInstance.update();
+      } else {
+        this.createChart(canvasId, labels, data);
+      }
     }
   }
 };
 </script>
+
+<style scoped>
+.chart-container {
+  background-color: #fc3434;
+  color: #fff;
+  padding: 20px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.chart-info {
+  width: 900px;
+  margin: 0 auto;
+  text-align: center;
+  padding: 20px 0;
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  position: relative;
+  height: 250px;
+}
+
+canvas {
+  max-width: 1000px;
+  max-height: 1000px;
+}
+</style>
